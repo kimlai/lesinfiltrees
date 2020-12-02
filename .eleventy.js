9101e7bcad818212808a8917aca3290f78f90235
kimlai <hello@kimlaitrinh.me>
2020-12-02 11:28:19 +0100

    Try out the netlify/cloudinary redirect trick
    https://twitter.com/philhawksworth/status/1328340868726726656

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,9 @@
 module.exports = config => {
   config.setTemplateFormats(["html", "css"]);
   config.addPassthroughCopy("images");
+  config.addFilter("imageUrl", url =>
+    process.env.CONTEXT === "production" ? `/cloudinaried/${url}` : url
+  );
   return {
     htmlTemplateEngine: "njk"
   };
