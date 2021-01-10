fcf0a78b809a4fb290f9e26746c2931d6d6a6f07
kimlai <hello@kimlaitrinh.me>
2021-01-10 13:33:21 +0100

    adjust bubbles animation

--- js/bubbles.js
+++ js/bubbles.js
@@ -1,4 +1,4 @@
-const BUBBLE_COUNT = 100;
+const BUBBLE_COUNT = 80;
 
 const randomInt = max => Math.floor(Math.random() * Math.floor(max));
 
@@ -7,10 +7,11 @@ for (let i = 0; i < BUBBLE_COUNT; i++) {
   element.classList.add("bubble");
   element.style.bottom = `${randomInt(20) * -1}px`;
   element.style.left = `${20 + randomInt(12)}rem`;
-  element.style.width = `${5 + randomInt(5)}px`;
-  element.style.height = `${5 + randomInt(10)}px`;
+  const width = 4 + randomInt(4);
+  element.style.width = `${width}px`;
+  element.style.height = `${width + randomInt(6)}px`;
   element.style["animation-delay"] = `${randomInt(500)}ms`;
-  element.style["animation-duration"] = `${450 + randomInt(50)}ms`;
+  element.style["animation-duration"] = `${450 + randomInt(100)}ms`;
   element.style.setProperty("--translateX", `${-3 + randomInt(6)}px`);
 
   document.body.appendChild(element);
