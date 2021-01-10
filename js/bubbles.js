5dae28d58eceebc76738ac145e7e0b315f5a0fdc
kimlai <hello@kimlaitrinh.me>
2021-01-10 15:02:44 +0100

    adjust rem values

--- js/bubbles.js
+++ js/bubbles.js
@@ -6,7 +6,7 @@ for (let i = 0; i < BUBBLE_COUNT; i++) {
   const element = document.createElement("div");
   element.classList.add("bubble");
   element.style.bottom = `${randomInt(20) * -1}px`;
-  element.style.left = `${20 + randomInt(12)}rem`;
+  element.style.left = `${18 + randomInt(12)}rem`;
   const width = 4 + randomInt(4);
   element.style.width = `${width}px`;
   element.style.height = `${width + randomInt(6)}px`;
