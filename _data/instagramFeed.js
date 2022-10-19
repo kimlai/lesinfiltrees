cbbce44d7d84f583fdd58c214de299529333bc66
kimlai <hello@kimlaitrinh.me>
2022-10-19 17:38:52 +0200

    do not fail the build if instagram fails

--- _data/instagramFeed.js
+++ _data/instagramFeed.js
@@ -4,26 +4,30 @@ const Cache = require("@11ty/eleventy-cache-assets");
 const excludedPhotos = ["17865418085150958"];
 
 module.exports = async () => {
-  const { data } = await Cache(
-    `https://graph.instagram.com/me/media?fields=id,caption,media_url,permalink&access_token=${process.env.INSTAGRAM_TOKEN}`,
-    {
-      duration: "1d",
-      type: "json"
+  try {
+    const { data } = await Cache(
+      `https://graph.instagram.com/me/media?fields=id,caption,media_url,permalink&access_token=${process.env.INSTAGRAM_TOKEN}`,
+      {
+        duration: "1d",
+        type: "json"
+      }
+    );
+
+    for (const image of data) {
+      const source = image.media_url;
+      const localUrl = `images/instagram/${image.id}.jpeg`;
+      const imageBuffer = await Cache(source, { duration: "1y", type: "buffer" });
+      fs.mkdirSync("images/instagram", { recursive: true });
+      fs.writeFileSync(localUrl, imageBuffer);
     }
-  );
 
-  for (const image of data) {
-    const source = image.media_url;
-    const localUrl = `images/instagram/${image.id}.jpeg`;
-    const imageBuffer = await Cache(source, { duration: "1y", type: "buffer" });
-    fs.mkdirSync("images/instagram", { recursive: true });
-    fs.writeFileSync(localUrl, imageBuffer);
+    return data
+      .filter(({ id }) => !excludedPhotos.includes(id))
+      .map(image => {
+        image.local_url = `images/instagram/${image.id}.jpeg`;
+        return image;
+      });
+  } catch(e) {
+    return [];
   }
-
-  return data
-    .filter(({ id }) => !excludedPhotos.includes(id))
-    .map(image => {
-      image.local_url = `images/instagram/${image.id}.jpeg`;
-      return image;
-    });
 };
